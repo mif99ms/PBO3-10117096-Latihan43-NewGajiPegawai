aef4970e83822e7d7ad4f49e67e06899ac90d7bd
mif99ms <msidqi86@gmail.com>
2018-10-22 23:14:02 +0700

    Initial commit

---
+++ src/pbo3/pkg10117096/latihan43/newgajipegawai/PBO310117096Latihan43NewGajiPegawai.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117096.latihan43.newgajipegawai;
+
+/**
+ *
+ * @author 
+ *  Nama         : Miftah Muhammad Sidqi
+ * NIM          : 10117096
+ * Kelas        : PBO3
+ * Deskripsi    : Program ini berisi program untuk menghitung gaji pegawai
+ */
+public class PBO310117096Latihan43NewGajiPegawai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GajiPegawai objG = new GajiPegawai();
+        objG.tampilData("", "", 0, 0, 0, 0);
+    }
+    
+}
